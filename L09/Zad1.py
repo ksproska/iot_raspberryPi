8fe1ce8f91c763c6c405261100798f4b2919cfc8
ksproska <kamila.sproska@gmail.com>
2021-12-09 17:11:06 +0100

    Initial commit

---
+++ L09/Zad1.py
@@ -0,0 +1,5 @@
+# Zadanie 1: Regulacja jasności świecenia diody enkoderem
+
+# Napisz program, który pozwala regulować przy pomocy enkodera jasność świecenia diody LED 1
+# w module niebieskich diod świecących. Do obsługi enkodera wykorzystaj zdarzenia (events).
+# Jest to przykładowe zadanie, które może być zmienione przez Prowadzącego zajęcia i połączone z innym.
---
+++ L09/Zad2.py
@@ -0,0 +1,8 @@
+# Zadanie 2: Odczyt parametrów środowiskowych z czujników DS18B20 oraz BME280 i ich
+# wizualizacja poprzez diody WS2812
+#
+# Napisz program, który wykorzystuje linijkę diod WS2812 do wizualizacji parametrów środowiskowych,
+# odczytanych z czujników DS18B20 oraz BME280, według schematu podanego przez Prowadzącego zajęcia.
+# Program może wykorzystywać interakcje przez konsolę tekstową i być sterowany poprzez przełączniki
+# przyciskane i enkoder.
+# Jest to przykładowe zadanie, które może być zmienione przez Prowadzącego zajęcia i połączone z innym.
