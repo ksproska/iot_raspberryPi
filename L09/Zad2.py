f9b1ca875500284304c5d08ca44777e8d0823cd1
ksproska <kamila.sproska@gmail.com>
2021-12-13 09:52:02 +0100

    Option handlers and corrections

--- L09/Zad2.py
+++ L09/Zad2.py
@@ -7,26 +7,26 @@ import busio
 import w1termsensor
 import adafruit_bme280.advanced as adafruit_bme280
 
-pixels = neopixel.NeoPiel(board.D18, 8, brightness=1.0/32, auto_write=False)
+# pixels = neopixel.NeoPiel(board.D18, 8, brightness=1.0 / 32, auto_write=False)
 
 
 class Color:
-    black = (0,0,0)
-    white = (255,255,255)
-    red = (255,0,0)
-    lime = (0,255,0)
-    blue = (0,0,255)
-    yellow = (255,255,0)
-    cyan = (0,255,255)
-    magenta = (255,0,255)
-    silver = (192,192,192)
-    grey = (128,128,128)
-    maroon = (128,0,0)
-    olive = (128,128,0)
-    green = (0,128,0)
-    purple = (128,0,128)
-    teal = (0,128,128)
-    navy = (0,0,128)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    lime = (0, 255, 0)
+    blue = (0, 0, 255)
+    yellow = (255, 255, 0)
+    cyan = (0, 255, 255)
+    magenta = (255, 0, 255)
+    silver = (192, 192, 192)
+    grey = (128, 128, 128)
+    maroon = (128, 0, 0)
+    olive = (128, 128, 0)
+    green = (0, 128, 0)
+    purple = (128, 0, 128)
+    teal = (0, 128, 128)
+    navy = (0, 0, 128)
     orange = (255, 128, 0)
 
 
@@ -37,38 +37,30 @@ class LedController:
             self.g = g
             self.b = b
             self.brightness = brightness
-        
+
         @classmethod
         def black(cls):
             return cls(*Color.black, 1)
 
         @property
         def color(self):
-            return self.r, self.g, self.b
+            return self.r, self.g, self.b, self.brightness
 
         @color.setter
-        def color(self, color_tuple):
-            if len(color_tuple) == 3:
-                self.r, self.g, self.b = color_tuple
-                self.brightness = 1
-            else:
-                self.r, self.g, self.b, self.brightness = color_tuple
+        def color(self, color_tuple: tuple[int, int, int]):
+            self.r, self.g, self.b = color_tuple
 
-    def __init__(self, brightness=1.0/32, auto_write=False):
+    def __init__(self, brightness=1.0 / 32, auto_write=False):
         self.__pixels = neopixel.NeoPixel(board.D18, 8, brightness=brightness, auto_write=auto_write)
         self.__colors = [self.InnerColor.black() for _ in self.__pixels]
-        
+        self.update_all()
+
     def update_all(self):
         for inx in range(self.__pixels):
             self.__pixels[inx] = self.__colors[inx].color
         self.__pixels.show()
 
-    def set_color(self, color_tuple: tuple, *inxs):
-        for inx in inxs:
-            self.__colors[inx].color = color_tuple
-        self.update_all()
-
-    def set_color_all(self, color_tuple: tuple):
+    def set_color_all(self, color_tuple: tuple[int, int, int]):
         for color in self.__colors:
             color.color = color_tuple
         self.update_all()
@@ -82,24 +74,22 @@ class LedController:
         self.__colors[5].color = Color.cyan
         self.__colors[6].color = Color.blue
         self.__colors[7].color = Color.purple
+        self.update_all()
 
     def display(self, val: float, start: float, end: float):
         max_one_led = (end - start) / 8
         full_leds = int((val - start) / max_one_led)
         brightness = (val - start - full_leds * max_one_led) / max_one_led
-        self.__assign(full_leds, brightness)
+        self.__assign_brightness(full_leds, brightness)
 
-    def __assign(self, full: int, brightness):
+    def __assign_brightness(self, number_of_full: int, value_of_not_full_brightness: float):
         for col in self.__colors:
             col.brightness = 0
 
-        for i in range(full):
+        for i in range(number_of_full):
             self.__colors[i].brightness = 1
-        self.__colors[full].brightness = brightness
+        self.__colors[number_of_full].brightness = value_of_not_full_brightness
         self.update_all()
-
-    def __getitem__(self, item):
-        return self.__pixels[item]
 
 
 class OutsideWorldHandler:
@@ -138,130 +128,146 @@ class ExerciseHandler:
     def __init__(self):
         self.led_controller = LedController()
         self.outside_word_handler = OutsideWorldHandler()
-        self.temp_start = self.outside_word_handler.temperature
-        self.temp_min = 10
-        self.temp_max = 40
-        self.altitude_min = 100
-        self.altitude_max = 160
-        self.pressure_min = 960
-        self.pressure_max = 1050
-
         self.current_option = 0
 
-        self.text_to_display = [f'Displaying temperature',
-                                f'Displaying humidity',
-                                f'Displaying altitude',
-                                f'Displaying pressure'
-                                ]
-
-        self.options = [self.option_temperature,
-                        self.humidity_act,
-                        self.altitude_act,
-                        self.pressure_act
-                        ]
-
-        self.encoder_options = [
-            lambda x: self.encoder_temperature(x),
-            lambda x: self.encoder_humidity(x),
-            lambda x: self.encoder_altitude(x),
-            lambda x: self.encoder_pressure(x)
-        ]
-
-        self.button_options = [
-            self.button_temperature,
-            self.button_humidity,
-            self.button_altitude,
-            self.button_pressure
-        ]
+        self.handlers: list[ExerciseHandler.OptionHandler] = \
+            [
+                self.TemperatureHandler(self.led_controller, self.outside_word_handler),
+                self.HumidityHandler(self.led_controller, self.outside_word_handler),
+                self.AltitudeHandler(self.led_controller, self.outside_word_handler),
+                self.PressureHandler(self.led_controller, self.outside_word_handler)
+            ]
 
     @property
-    def get_current(self):
-        return self.options[self.current_option]
+    def current_handler(self):
+        return self.handlers[self.current_option]
 
-    # temperature _______________________________________________________________________________________________
-    def option_temperature(self):
-        temp = self.outside_word_handler.temperature
-        self.led_controller.rainbow()
-        self.led_controller.display(temp, self.temp_min, self.temp_max)
+    @property
+    def current_activity(self):
+        return self.current_handler.activity
 
-    def encoder_temperature(self, is_right):
-        if is_right:
-            self.temp_max += 1
-        else:
-            self.temp_max -= 1
-        print(f'Temperature range is: {self.temp_min} to {self.temp_max} ^C')
+    @property
+    def current_encoder(self):
+        return lambda x: self.current_handler.encoder(x)
 
-    def button_temperature(self):
-        new_min_temp = input(f'Current min temp is {self.temp_min}. Set new one: ')
-        self.temp_min = int(new_min_temp)
+    @property
+    def current_button(self):
+        return self.current_handler.button
 
-    # humidity ___________________________________________________________________________________________________
-    def humidity_act(self):
-        hum = self.outside_word_handler.humidity
-        self.led_controller.set_color_all(Color.blue)
-        self.led_controller.display(hum, 0, 100)
+    class OptionHandler:
+        def __init__(self, led_controller, outside_word_handler):
+            self.led_controller = led_controller
+            self.outside_word_handler = outside_word_handler
 
-    def encoder_humidity(self, is_right):
-        pass
+        def __str__(self):
+            return f'Current running: {self.__class__.__name__}'
 
-    def button_humidity(self):
-        pass
+        def activity(self):
+            pass
 
-    # altitude ____________________________________________________________________________________________________
-    def altitude_act(self):
-        alt = self.outside_word_handler.altitude
-        self.led_controller.set_color_all(Color.yellow)
-        self.led_controller.display(alt, self.altitude_min, self.altitude_max)
+        def encoder(self, is_right: bool):
+            print(f'Encoder from: {self.__class__.__name__}')
+            pass
 
-    def encoder_altitude(self, is_right):
-        pass
+        def button(self):
+            print(f'Button from: {self.__class__.__name__}')
+            pass
 
-    def button_altitude(self):
-        pass
+    class TemperatureHandler(OptionHandler):
+        def __init__(self, led_controller, outside_word_handler):
+            super().__init__(led_controller, outside_word_handler)
+            self.temp_start = self.outside_word_handler.temperature
+            self.temp_min = 10
+            self.temp_max = 40
 
-    # pressure ___________________________________________________________________________________________________
-    def pressure_act(self):
-        press = self.outside_word_handler.pressure
-        self.led_controller.set_color_all(Color.orange)
-        self.led_controller.display(press, self.pressure_min, self.pressure_max)
+        def activity(self):
+            super().activity()
+            temp = self.outside_word_handler.temperature
+            print(f'Current temperature: {temp}')
+            self.led_controller.rainbow()
+            self.led_controller.display(temp, self.temp_min, self.temp_max)
 
-    def encoder_pressure(self, is_right):
-        pass
+        def encoder(self, is_right: bool):
+            super().encoder(is_right)
+            if is_right:
+                self.temp_max += 1
+            else:
+                self.temp_max -= 1
+            print(f'Temperature range is: {self.temp_min} to {self.temp_max} °C')
 
-    def button_pressure(self):
-        pass
+        def button(self):
+            super().button()
+            new_min_temp = input(f'Current min temp is {self.temp_min}. Set new one: ')
+            self.temp_min = int(new_min_temp)
+
+    class HumidityHandler(OptionHandler):
+        def __init__(self, led_controller, outside_word_handler):
+            super().__init__(led_controller, outside_word_handler)
+
+        def activity(self):
+            super().activity()
+            hum = self.outside_word_handler.humidity
+            print(f'Current humidity: {hum}')
+            self.led_controller.set_color_all(Color.blue)
+            self.led_controller.display(hum, 0, 100)
+
+    class AltitudeHandler(OptionHandler):
+        def __init__(self, led_controller, outside_word_handler):
+            super().__init__(led_controller, outside_word_handler)
+            self.altitude_min = 100
+            self.altitude_max = 160
+
+        def activity(self):
+            super().activity()
+            alt = self.outside_word_handler.altitude
+            print(f'Current altitude: {alt}')
+            self.led_controller.set_color_all(Color.yellow)
+            self.led_controller.display(alt, self.altitude_min, self.altitude_max)
+
+    class PressureHandler(OptionHandler):
+        def __init__(self, led_controller, outside_word_handler):
+            super().__init__(led_controller, outside_word_handler)
+            self.pressure_min = 960
+            self.pressure_max = 1050
+
+        def activity(self):
+            super().activity()
+            press = self.outside_word_handler.pressure
+            print(f'Current pressure: {press}')
+            self.led_controller.set_color_all(Color.orange)
+            self.led_controller.display(press, self.pressure_min, self.pressure_max)
 
     # menus _____________________________________________________________________________________________________
-    def menu_forward(self):
-        self.current_option = (self.current_option + 1) % len(self.options)
-        print(self.text_to_display[self.current_option])
+    def set_next(self):
+        self.current_option = (self.current_option + 1) % len(self.handlers)
+        print(self.current_handler)
 
-    def menu_backward(self):
-        self.current_option = (self.current_option + len(self.options) - 1) % len(self.options)
+    def set_prev(self):
+        self.current_option = (self.current_option + len(self.handlers) - 1) % len(self.handlers)
 
-    def leftPinCallback(self):
+    def encoder_left_callback(self):
         if GPIO.input(encoderRight) == 0:
-            self.encoder_options[self.current_option](False)
+            self.current_encoder(False)
 
-    def rightPinCallback(self):
+    def encoder_right_callback(self):
         if GPIO.input(encoderLeft) == 0:
-            self.encoder_options[self.current_option](True)
+            self.current_encoder(True)
 
-    def green_button_menu(self):
-        self.button_options[self.current_option]()
+    def green_button_callback(self):
+        self.current_button()
 
 
 exHand = ExerciseHandler()
 
 
 def setup():
-    GPIO.add_event_detect(buttonRed, GPIO.FALLING, callback=exHand.menu_forward, bouncetime=200)
-    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=exHand.green_button_menu, bouncetime=200)
-    GPIO.add_event_detect(encoderLeft, GPIO.RISING, callback=exHand.leftPinCallback, bouncetime=200)
-    GPIO.add_event_detect(encoderRight, GPIO.RISING, callback=exHand.rightPinCallback, bouncetime=200)
+    GPIO.add_event_detect(buttonRed, GPIO.FALLING, callback=exHand.set_next, bouncetime=200)
+    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=exHand.green_button_callback, bouncetime=200)
+    GPIO.add_event_detect(encoderLeft, GPIO.RISING, callback=exHand.encoder_left_callback, bouncetime=200)
+    GPIO.add_event_detect(encoderRight, GPIO.RISING, callback=exHand.encoder_right_callback, bouncetime=200)
 
 
 if __name__ == '__main__':
     setup()
     while True:
-        exHand.get_current()
+        exHand.current_activity()
